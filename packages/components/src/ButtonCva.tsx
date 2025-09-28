6aa7526d3d08b5781467c6e3d77837d2740265b3
fengliang12 <1724205587@qq.com>
2025-09-28 16:44:23 +0800

    test

--- packages/components/src/ButtonCva.tsx
+++ packages/components/src/ButtonCva.tsx
@@ -3,5 +3,5 @@ import React from "react";
 export const ButtonCva = () => {
   React.useEffect(() => {}, []);
 
-  return <div className="ButtonCva">mono repo</div>;
+  return <div className="ButtonCva">mono repo1</div>;
 };
