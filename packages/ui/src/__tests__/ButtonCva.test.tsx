4dbd2d6e17c7ebc1d263570d81c317865bbbbfec
juzhiqiang <1020814597@qq.com>
2025-10-03 01:27:04 +0800

    update: 调整ui组件结构

--- packages/ui/src/__tests__/ButtonCva.test.tsx
+++ packages/ui/src/__tests__/ButtonCva.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it, jest } from "@jest/globals";
-import { ButtonCva } from "./ButtonCva";
+import { ButtonCva } from "../Button/ButtonCva";
 
 describe("ButtonCva", () => {
   describe("Rendering", () => {
